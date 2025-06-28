2653fe2f0ea6e995a993f686a87db6ef6e27ac40
bhashkar2004 <marisabhashkar2004@gmail.com>
2025-06-28 22:03:01 +0530

    Add files via upload

---
+++ Tic Tac Toe/TicTacToe.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    static char[][] board = {
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}
+    };
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char currentPlayer = 'X';
+
+        while (true) {
+            printBoard();
+            System.out.println("Player " + currentPlayer + ", enter your move (row and column: 1-3): ");
+            int row = scanner.nextInt() - 1;
+            int col = scanner.nextInt() - 1;
+
+            if (isValidMove(row, col)) {
+                board[row][col] = currentPlayer;
+
+                if (checkWinner(currentPlayer)) {
+                    printBoard();
+                    System.out.println("Player " + currentPlayer + " wins!");
+                    break;
+                } else if (isDraw()) {
+                    printBoard();
+                    System.out.println("It's a draw!");
+                    break;
+                }
+
+                
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            } else {
+                System.out.println("Invalid move! Try again.");
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static void printBoard() {
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
+    public static boolean isValidMove(int row, int col) {
+        return (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ');
+    }
+
+    public static boolean checkWinner(char player) {
+        
+        for (int i = 0; i < 3; i++)
+            if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) ||
+                (board[0][i] == player && board[1][i] == player && board[2][i] == player))
+                return true;
+
+        return (board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
+               (board[0][2] == player && board[1][1] == player && board[2][0] == player);
+    }
+
+    public static boolean isDraw() {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (board[i][j] == ' ')
+                    return false;
+        return true;
+    }
+}
